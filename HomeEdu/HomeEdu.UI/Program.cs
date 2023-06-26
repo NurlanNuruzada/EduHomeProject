c2c43445019359f424f0a82d2a1687e177ddc926
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-06-27 01:55:32 +0400

    AdminPanelAded

--- HomeEdu/HomeEdu.UI/Program.cs
+++ HomeEdu/HomeEdu.UI/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapControllerRoute(
+    name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+app.MapControllerRoute(
     name: "Default",
     pattern: "{controller=Home}/{action=Index}/{Id?}");
 app.Run();
