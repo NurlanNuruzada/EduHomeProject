e7d0c445220e1da6ffb44c6596fcece695ebc75f
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-06-28 23:37:57 +0400

    blog detail added

--- HomeEdu/HomeEdu.UI/Controllers/BlogDetailController.cs
+++ HomeEdu/HomeEdu.UI/Controllers/BlogDetailController.cs
@@ -1,13 +1,27 @@
 ﻿using HomeEdu.Core.Entities;
+using HomeEdu.DataAccess.Context;
+using HomeEdu.UI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Versioning;
 
 namespace HomeEdu.UI.Controllers
 {
     public class BlogDetailController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public BlogDetailController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int Id)
+        {
+            BlogVM homeVM = new()
+            {
+                Blogs = await _context.Blogs.Where(s => s.Id == Id).ToListAsync()
+            };
+            return View(homeVM);
         }
     }
 }
