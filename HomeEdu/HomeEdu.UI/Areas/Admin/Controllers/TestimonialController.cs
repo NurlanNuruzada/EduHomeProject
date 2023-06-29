6f74bfd3b84a85f0523d6de23e8a3875e6f90fa4
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-06-29 17:49:18 +0400

    addedTestimonialsIndexToAdmin

---
+++ HomeEdu/HomeEdu.UI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HomeEdu.Core.Entities;
+using HomeEdu.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeEdu.UI.Areas.Admin.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _env;
+
+        public TestimonialController(AppDbContext context, IMapper mapper, IWebHostEnvironment env)
+        {
+            _context = context;
+            _mapper = mapper;
+            _env = env;
+        }
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+
+            List<testimonial> testimonials = await _context.testimonials.ToListAsync();
+            return View(testimonials);
+        }
+    }
+}
