4e000345dc2c979341158d4be571ebfafb0d01a5
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-06-29 18:08:34 +0400

    addedInsetToTestimonial

--- HomeEdu/HomeEdu.UI/Areas/Admin/Controllers/TestimonialController.cs
+++ HomeEdu/HomeEdu.UI/Areas/Admin/Controllers/TestimonialController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using HomeEdu.Core.Entities;
 using HomeEdu.DataAccess.Context;
+using HomeEdu.UI.Areas.Admin.ViewModels.SliderViewModel;
+using HomeEdu.UI.Areas.Admin.ViewModels.TestimoniaViewModel;
+using HomeEdu.UI.Helpers.Extentions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,6 +27,40 @@ namespace HomeEdu.UI.Areas.Admin.Controllers
 
             List<testimonial> testimonials = await _context.testimonials.ToListAsync();
             return View(testimonials);
+        }
+        [Area("Admin")]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Catagories = await _context.testimonials.ToListAsync();
+            return View();
+        }
+        [Area("Admin")]
+        [HttpPost]
+        [ActionName("Create")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(TestimoniaVM testimoniaVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(testimoniaVM);
+            }
+            if (!testimoniaVM.Image.CheckFileFormat("image"))
+            {
+                ModelState.AddModelError("Image", "Sellect Correct Format!");
+                return View(testimoniaVM);
+            }
+            if (!testimoniaVM.Image.CheckFileLength(300))
+            {
+                ModelState.AddModelError("Image", "Size Must be less than 300 kb");
+                return View(testimoniaVM);
+            }
+            string filePath = await testimoniaVM.Image.CopyFileAsync(_env.WebRootPath, "assets", "img", "slider");
+            testimonial testimonial = _mapper.Map<testimonial>(testimoniaVM);
+            testimonial.ImagePath = filePath;
+            await _context.testimonials.AddAsync(testimonial);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ HomeEdu/HomeEdu.UI/Mapper/TestimonialProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HomeEdu.Core.Entities;
+using HomeEdu.UI.Areas.Admin.ViewModels.SliderViewModel;
+using HomeEdu.UI.Areas.Admin.ViewModels.TestimoniaViewModel;
+
+namespace HomeEdu.UI.Mapper
+{
+    public class TestimonialProfile :Profile
+    {
+        public TestimonialProfile()
+        {
+            CreateMap<TestimoniaVM, testimonial>().ReverseMap();
+        }
+    }
+}
