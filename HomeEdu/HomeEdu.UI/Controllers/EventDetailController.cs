2e36ceebb7f4ea2d772424482c3b1e25615124b8
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-07-01 00:37:52 +0400

    addedEventDetailAndSpeakerToIndex

---
+++ HomeEdu/HomeEdu.UI/Controllers/EventDetailController.cs
@@ -0,0 +1,38 @@
+﻿using HomeEdu.DataAccess.Context;
+using HomeEdu.UI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeEdu.UI.Controllers
+{
+    public class EventDetailController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EventDetailController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int Id)
+        {
+            var @event = await _context.Events.FindAsync(Id);
+            var eventDetails = await _context.EventDetails
+                .Where(e => e.EventId == Id)
+                .ToListAsync();
+            var speakers = await _context.Speakers
+       .Include(s => s.EventDetail)
+       .Where(s => s.EventDetail.EventId == Id)
+       .ToListAsync();
+
+            var eventDetailVM = new EventDetailViewModel
+            {
+                Event = @event,
+                eventDetails = eventDetails,
+                 Speakers = speakers
+            };
+
+            return View(eventDetailVM);
+        }
+    }
+}
---
+++ HomeEdu/HomeEdu.UI/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeEdu.UI.ViewModels
+{
+    public class EventDetailViewModel
+    {
+        public Event Event { get; set; }
+        public IEnumerable<EventDetail> eventDetails { get; set; } = null!;
+        public IEnumerable<Speaker> Speakers { get; set; }
+    }
+}
