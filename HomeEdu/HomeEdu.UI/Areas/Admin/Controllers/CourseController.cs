de874223f35f5e72c921c5d5d8d59d5305df7f64
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-07-03 04:57:37 +0400

    courseAddedToAdmin

---
+++ HomeEdu/HomeEdu.UI/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HomeEdu.Core.Entities;
+using HomeEdu.DataAccess.Context;
+using HomeEdu.UI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeEdu.UI.Areas.Admin.Controllers
+{
+
+    public class CourseController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CourseController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+            List<Course> courses = await _context.Courses.Include(e => e.CourseCatagory).ToListAsync();
+            return View(courses);
+        }
+
+    }
+}
---
+++ HomeEdu/HomeEdu.UI/Areas/Admin/ViewModels/CourseViewModel/CourseViewModel.cs
@@ -0,0 +1,42 @@
+﻿using HomeEdu.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeEdu.UI.Areas.Admin.ViewModels.CourseViewModel;
+
+public class CourseViewModel
+{
+    [Required, MaxLength(50)]
+    public string? Title { get; set; }
+    [Required, MaxLength(150)]
+    public string? Description { get; set; }
+
+    public int CourseCatagoryId { get; set; }
+    public CourseCatagory CourseCatagory { get; set; } = null!;
+
+    [Required]
+    public string ImagePath { get; set; } = null!;
+
+    public int CourseDetailId { get; set; }
+    [Required]
+    public CourseDetail CourseDetail { get; set; } = null!;
+    [Required]
+    public int CourseFee { get; set; }
+
+    [Required, StringLength(900)]
+    public string AboutCourse { get; set; } = null!;
+
+    [Required, StringLength(900)]
+    public string HowToApply { get; set; } = null!;
+
+    [Required, StringLength(900)]
+    public string Certification { get; set; } = null!;
+    [Required]
+    public TimeSpan Duration { get; set; }  
+    [Required]
+    public DateTime Starts { get; set; }
+    [Required]
+    public TimeSpan ClassDuration { get; set; }
+    public ICollection<Assesments> Assessments { get; set; }
+    public ICollection<SkillLevel> SkillLevels { get; set; }
+    public ICollection<Language> Languages { get; set; }
+}
