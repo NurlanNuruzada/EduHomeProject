69282293ce303e3f66861c1bc0fe7111b7150f03
Nurlan <126083012+NurlanNuruzada@users.noreply.github.com>
2023-07-05 03:42:53 +0400

    beforeAddLogin

---
+++ HomeEdu/HomeEdu.UI/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using HomeEdu.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HomeEdu.DataAccess.Context;
+using AutoMapper;
+using HomeEdu.UI.ViewModels;
+
+namespace HomeEdu.UI.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public CoursesController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            CoursesVM coursesVM = new()
+            {
+                Courses = await _context.Courses.ToListAsync(),
+                Blogs = await _context.Blogs.ToListAsync()
+            };
+            return View(coursesVM);
+        }
+    }
+}
---
+++ HomeEdu/HomeEdu.UI/ViewModels/CoursesVM.cs
@@ -0,0 +1,11 @@
+﻿using HomeEdu.Core.Entities;
+
+namespace HomeEdu.UI.ViewModels
+{
+    public class CoursesVM
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Blog> Blogs { get; set; }
+
+    }
+}
