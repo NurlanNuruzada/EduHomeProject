f22edc88eb451863e45aac7011fc17c62be6dd2c
unknown <nurlangn@code.edu.az>
2023-07-17 03:50:45 +0400

    teacherDone

---
+++ HomeEdu/HomeEdu.UI/Controllers/TeachersController.cs
@@ -0,0 +1,36 @@
+﻿using HomeEdu.Core.Entities;
+using HomeEdu.DataAccess.Context;
+using HomeEdu.UI.ViewModels;
+using HomeEdu.UI.ViewModels.TeacherViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeEdu.UI.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Teachers> Teachers = await _context.Teachers.ToListAsync();
+            return View(Teachers);
+        }
+        public async Task<IActionResult> TeacherDetails(int id)
+        {
+            Teachers? teacher = await _context.Teachers
+               .Where(e => e.Id == id)
+               .Include(e => e.TeacherDetails)
+               .FirstOrDefaultAsync();
+            if(teacher is null)
+            {
+                return NotFound();
+            }
+            return View(teacher);
+        }
+    }
+}
---
+++ HomeEdu/HomeEdu.UI/ViewModels/TeacherViewModels/TeacherVM.cs
@@ -0,0 +1,10 @@
+﻿using HomeEdu.Core.Entities;
+
+namespace HomeEdu.UI.ViewModels.TeacherViewModels
+{
+    public class TeacherVM
+    {
+       public  IEnumerable<Teachers>? teachers { get; set; }
+       public  IEnumerable<TeacherDetails>? TeacherDetails { get; set; }
+    }
+}
